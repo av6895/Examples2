ae2a072bcd6c4ea7a1803e7fdb9ead6713df00a9
Anastasia <sadness1995@yandex.ru>
2023-03-14 11:56:29 +0300

    Лекция 7

--- Examples/Example001_HelloConsole/Program.cs
+++ Examples/Example001_HelloConsole/Program.cs
@@ -1,1 +1,95 @@
-﻿Console.WriteLine(" 2 Hello, World!");
+﻿// string path = "/Users/sadne/OneDrive/Рабочий стол/GB/Example/Examples/Example002_HelloUser";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+
+// FileInfo[] fi = di.GetFiles();
+
+// for(int i = 0; i < fi.Length; i++)
+// {
+//   System.Console.WriteLine(fi[i].Name);
+// }
+
+
+// void CatalogInfo(string path, string indent = "")
+// {
+//   DirectoryInfo[] catalog = catalog.GetDirectories();
+//   for(int i = 0; i < catalog.Length; i++)
+//   {
+//     Console.WriteLine($"{indent}{catalog[i].Name}");
+//     CatalogInfo(catalog[i].FullName, indent + " ");
+//   }
+
+//   FileInfo[] files = catalog.GetFiles();
+//   for(int i = 0; i < files.Length; i++)
+//   {
+//     Console.WriteLine($"{indent}{files[i].Name}");
+//   }
+// }
+// string path = @"/Users/sadne/OneDrive/Рабочий стол/GB/Example";
+// CatalogInfo(path);
+
+
+
+// // НЕ РАБОТАЕТ! ОШИБКИ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // игра в пирамидки 
+
+// void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
+// {
+//   if(count > 1) Towers(with, some, on, count - 1);
+//   Console.WriteLine($"{with} >> {on}");
+//   if(count > 1) Towers(some, on, with, count - 1);
+// }
+
+// Towers();
+
+
+
+
+
+
+
+
+// //Обход разных структур 
+// // ((4-2)*(1+3))/10
+// // string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// // //                 0    1    2    3     4    5    6    7    8    9    10   11
+
+// string emp = String.Empty;
+// string[] tree = {emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// void InOrderTraversal(int pos = 1)
+// {
+//   if(pos < tree.Length)
+//   {
+//     int left = 2 * pos;
+//     int right = 2 * pos + 1;
+//     if(left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+//     Console.WriteLine(tree[pos]);
+//     if(right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+//   }
+// }
+// InOrderTraversal();
+
+
+
+
+
+
+
+
---
+++ Examples/Лекция_7/Program.cs
@@ -0,0 +1,174 @@
+﻿//смотрим как решаются задачи сначала используя ветвления и циклы, потом при помощи рекурсии
+
+
+// //собрать строку с числами от a до b, a<=b
+// // 1
+// string NumbersFor(int a, int b)
+// {
+//   string result = string.Empty;
+//   for(int i = a; i <= b; i++)
+//   {
+//     result += $"{i} ";
+//   }
+//   return result;
+// }
+
+// // 2
+// string NumbersRec(int a, int b)
+// {
+//   if(a <= b) return $"{a} " + NumbersRec(a + 1, b);
+//   else return String.Empty;
+// }
+
+// Console.WriteLine(NumbersFor(1, 10));
+// Console.WriteLine(NumbersRec(1, 10));
+
+
+
+
+
+
+
+// //собрать строку с числами от a до b, a>=b
+// // 1
+// string NumbersFor(int a, int b)
+// {
+//   string result = String.Empty;
+//   for(int i = a; i >= b; i--)
+//   {
+//     result += $"{i} ";
+//   }
+//   return result;
+// }
+
+// // 2
+// string NumbersRec(int a, int b)
+// {
+//   if(a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+//   else return String.Empty;
+// }
+
+// Console.WriteLine(NumbersFor(1, 10));    //не работает
+// Console.WriteLine(NumbersRec(1, 10));
+
+
+
+
+
+
+
+// //сумма чисел от 1 до n
+
+// // 1
+// int SumFor(int n)
+// {
+//   int result = 0;
+//   for(int i = 1; i <= n; i++) result += i;
+//   return result;
+// }
+
+// // 2
+// int SumRec(int n)
+// {
+//   if(n == 0) return 0;
+//   else return n + SumRec(n - 1);
+// }
+
+// Console.WriteLine(SumFor(10));
+// Console.WriteLine(SumRec(10));
+
+
+
+
+
+
+
+// // факториал числа
+
+// // 1
+// int FactorialFor(int n)
+// {
+//   int result = 1;
+//   for(int i = 1; i <= n; i++) result *= i;
+//   return result;
+// }
+
+// // 2
+// int FactorialRec(int n)
+// {
+//   if(n == 1) return 1;
+//   else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialFor(10));
+// Console.WriteLine(FactorialRec(10));
+
+
+
+
+
+
+
+
+// // вычислить a в степени n
+
+// // 1
+// int PowerFor(int a, int n)
+// {
+//   int result = 1;
+//   for(int i = 1; i <= n; i++) result *= a;
+//   return result;
+// }
+
+// // 2
+// int PowerRec(int a, int n)
+// {
+//   //return n == 0 ? 1 : PowerRec(a, n - 1) * a;   - можно записать все одной строкой
+//   if(n == 0) return 1;
+//   else return PowerRec(a, n - 1) * a;
+// }
+
+// //  3
+// int PowerRecMath(int a, int n)
+// {
+//   if(n == 0) return 1;
+//   else if(n % 2 == 0) return PowerRecMath(a * a, n / 2);
+//   else return PowerRecMath(a, n - 1) * a;
+// }
+
+// Console.WriteLine(PowerFor(2, 10));
+// Console.WriteLine(PowerRec(2, 10));
+// Console.WriteLine(PowerRecMath(2, 10));
+
+
+
+
+
+
+
+// Перебор слов. 
+// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
+// Покажите все слова, состоящие из T букв, которые можно построить из букв этого алфавита
+
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//   if (length == word.Length)
+//   {
+//     Console.WriteLine($"{n++} {new String(word)}"); return;
+//   }
+  
+//   for (int i = 0; i < alphabet.Length; i++)
+//   {
+//     word[length] = alphabet[i];
+//     FindWords(alphabet, word, length + 1);
+//   }
+// }
+// FindWords(“аисв”, new char[5]);   //??????????????
+
+
+
+
+
+
+
+
